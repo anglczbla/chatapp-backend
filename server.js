46356f21d49d9c263a99c74069bb1614295cc517
angel <angelicaizabella90@gmail.com>
2025-08-02 22:56:45 +0700

    server.js updated for vercel

--- server.js
+++ server.js
@@ -1,50 +1,45 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import  cors from "cors";
+import cors from "cors";
 import http from "http";
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoute.js';
-import messageRouter from './routes/messageRoute.js';
-import { Server } from 'socket.io';
-import { Socket } from 'dgram';
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoute.js";
+import messageRouter from "./routes/messageRoute.js";
+import { Server } from "socket.io";
+import { Socket } from "dgram";
 
 const app = express();
-const server = http.createServer(app)
-
+const server = http.createServer(app);
 
 // initialize socket.io  server
 export const io = new Server(server, {
-    cors:{origin: "*"}
-})
+  cors: { origin: "*" },
+});
 
 // store  online users
 export const userSocketMap = {}; // {userId: socketId}
 
 // socket.io connection halder
-io.on("connection", (socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User connected", userId);
-    if (userId) userSocketMap[userId]= socket.id;
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected", userId);
+  if (userId) userSocketMap[userId] = socket.id;
 
-    // emit online users to all connected client
+  // emit online users to all connected client
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected", userId);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-        
-    })
-
-})
-
+  });
+});
 
 // middleware setup
-app.use(express.json({limit: '4mb'}))
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-
-app.use("/api/status", (req,res)=>res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
@@ -52,6 +47,10 @@ app.use("/api/messages", messageRouter);
 // connect to db
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=> console.log("Server is running on PORT" + PORT)
-);
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT" + PORT));
+}
+
+// export server for Vercel
+export default server;
